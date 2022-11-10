165b77e0d7e5cec1d9cdc88b733be4cdebe90b7a
Andres Nazzari <lordcliff@gmail.com>
2022-11-10 16:20:09 +0900

    🚧 PROGRESS: add get discount

---
+++ server/src/controllers/discount.controller.js
@@ -0,0 +1,21 @@
+import { validationResult } from 'express-validator';
+
+export default class DiscountController {
+  constructor({ discountService }) {
+    this.discountService = discountService;
+
+    this.getDiscounts = this.getDiscounts.bind(this);
+  }
+
+  async getDiscounts(req, res) {
+    try {
+      const data = await this.discountService.getDiscounts();
+      const discounts = data.map((item) => item.discount);
+      const uniqueDiscounts = [...new Set(discounts)];
+      res.status(200).json({ discounts: uniqueDiscounts });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send(`Server Error: ${error.message}`);
+    }
+  }
+}
