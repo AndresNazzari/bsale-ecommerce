bc2ab8d8385af754e3a7b9201654b1cbc90ccc81
Andres Nazzari <lordcliff@gmail.com>
2022-11-10 16:25:45 +0900

    🚑 HOTFIX: missing parameter

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 import server from './server.js';
-
 import cors from 'cors';
 import { depInySetup, container } from './src/config/dep-inyection-setup.js';
 import { config } from './src/config/config.js';
-const app = server(config, cors, depInySetup, container);
+
+const app = server(cors, depInySetup, container);
 
 app.listen(config.PORT || 3000, () => {
   console.log(`🚀 Server started on port  ${config.PORT} `);
